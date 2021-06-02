2def00f9406cc6932b5055209c2a38e0feebfebf
Frank Parra <panf9188@gmail.com>
2021-06-01 19:26:39 -0500

    Ejercicio 2 de la clase

---
+++ Clase_02/ejercicio_01.rb
@@ -0,0 +1,9 @@
+puts "Ingrese su edad: "
+
+edad = gets.chomp.to_i
+
+if edad < 18
+  puts "Ud es menor de edad"
+else 
+  puts "Ud es mayor de edad"
+end
---
+++ Clase_02/ejercicio_02.rb
@@ -0,0 +1,9 @@
+puts "Ingrese un número"
+
+numero = gets.chomp.to_i
+
+if numero > 5
+  puts "El número #{numero} es mayor a 5"
+elsif numero == 5
+  puts "el número es igual a 5"
+end
